2e36dc2f9a1853740e6e61cfb6dd51dcff98e45e
Jonathan Baggott <jonathan.baggott82@gmail.com>
2022-04-13 22:10:45 +0100

    This is a new commit for what I originally planned to be added.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using CodeMeetupRSVP.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace CodeMeetupRSVP.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View();
+        }
+
+    }
+}
