8c3cafdd98eda83392eba7ff403a390926bac480
Jonathan Baggott <jonathan.baggott82@gmail.com>
2022-04-14 16:04:42 +0100

    Added validation code as well as a style.css file which overwrites the input-validation-error class.

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -1,10 +1,20 @@
-﻿namespace CodeMeetupRSVP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeMeetupRSVP.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string? Phone { get; set; }
-        public bool WillAttend { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
     }
 }
